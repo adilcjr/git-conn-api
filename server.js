fb169e4a236800e33f0cd361c2c9ac1a7bfcc27d
Adil Calomeno Jr <adil.calomeno.jr@gmail.com>
2019-04-06 22:58:22 -0300

    Enable CORS

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    req.header("Access-Control-Allow-Origin", "*");
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    req.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
 
