2a7bf3b8470468757a73f7b1296c574795e87d81
Adil Calomeno Jr <adil.calomeno.jr@gmail.com>
2019-04-07 00:54:53 -0300

    Trying to solve the CORS problem

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ const path = require('path');
 var bodyparser = require("body-parser");
 
 var app = express();
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}));
+//app.use(bodyparser.json());
+//app.use(bodyparser.urlencoded({extended:false}));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    req.header("Access-Control-Allow-Origin", "*");
-    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    req.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
 
