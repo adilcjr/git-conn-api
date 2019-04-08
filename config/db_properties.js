95de25f44298fc114386d8619a0251b438e37298
Adil Calomeno Jr <adil.calomeno.jr@gmail.com>
2019-04-07 21:51:52 -0300

    Hiding database details

--- config/db_properties.js
+++ config/db_properties.js
@@ -1,8 +1,4 @@
-module.exports = {
-    // host: "us-cdbr-iron-east-03.cleardb.net",
-    // user: "b0d4acaa6bdbbc",
-    // password: "08f85c87",
-    // dbName: "heroku_7b24a25928ac47a"
+module.exports = {    
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
